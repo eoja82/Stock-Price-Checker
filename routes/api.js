1faa1672b69d744a8a26f75b08b5d5374b145da5
Glitch (chalk-typhoon) <none>
2019-07-24 21:14:12 +0000

    🙏⛸ Checkpoint ./routes/api.js:5079832/243 ./views/index.html:5079832/10

--- routes/api.js
+++ routes/api.js
@@ -17,15 +17,21 @@ mongoose.set('useFindAndModify', false);  //to use findOneAndUpdate
 
 module.exports = function (app) {
   
-  var stockSchema = new mongoose.schema({
+  var stockSchema = new mongoose.Schema({
     stock: String,
     price: Number,
     ip: [String],
     likes: Number
-  })
+  });
+  
+  var Stock = mongoose.model("Stock", stockSchema);
 
   app.route('/api/stock-prices')
     .get(function (req, res){
+    var json;
+    var stock = req.query.stock; //get one stock price
+    var stock1of2 = req.query.stock1of2;  //compare stock prices
+    var stock2of2 = req.query.stock2of2;
     
       
     });
