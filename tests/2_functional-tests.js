ffb024519dc922b9dae69abc98b59619303388c5
Glitch (chalk-typhoon) <none>
2019-07-29 18:26:02 +0000

    🏭🏓 Checkpoint ./tests/2_functional-tests.js:5079832/602

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -65,10 +65,9 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.equal(res.body.stockData[0].stock, "GOOG")
-          assert.equal(res.body.stockData[0].stock, "GOOG")
-          assert.property(res.body.stockData, "price");
-          assert.property(res.body.stockData, "likes");
-          //not done
+          assert.equal(res.body.stockData[1].stock, "MSFT")
+          assert.property(res.body.stockData[0], "price");
+          assert.property(res.body.stockData[0], "rel_likes");
           done();
         });
       });
@@ -76,13 +75,13 @@ suite('Functional Tests', function() {
       test('2 stocks with like', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock1: })
+        .query({stock1: "goog", stock2: "msft", like: true})
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.property(res.body.stockData, "stock");
-          assert.property(res.body.stockData, "price");
-          assert.property(res.body.stockData, "likes");
-          //not done
+          assert.equal(res.body.stockData[0].stock, "GOOG")
+          assert.equal(res.body.stockData[1].stock, "MSFT")
+          assert.property(res.body.stockData[0], "price");
+          assert.property(res.body.stockData[0], "rel_likes");
           done();
         });
       });
