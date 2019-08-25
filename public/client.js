339d1e5e8f84978b87374fbde0cc298d1536b0c1
Glitch (chalk-typhoon) <none>
2019-08-25 20:43:16 +0000

    👋🌎 Checkpoint ./views/index.html:5079832/1272 ./public/style.css:5079832/30 ./public/client.js:5079832/234

---
+++ public/client.js
@@ -0,0 +1,8 @@
+let hideExamples = () => {
+  let x = document.getElementById("examples");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.sytyle.display = "none"
+  }
+};
