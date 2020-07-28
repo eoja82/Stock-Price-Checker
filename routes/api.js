8c0db7bd782da2780d58f1565d64af46003bd78d
Glitch (chalk-typhoon) <none>
2020-07-28 18:13:23 -0500

    update promise error handling

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,6 @@
 
 'use strict';
 
-var expect      = require('chai').expect;
-var MongoClient = require('mongodb');
-var shortid     = require("shortid");
 var mongoose    = require('mongoose');
 var request     = require("request");
 
@@ -29,107 +26,137 @@ module.exports = function (app) {
   var Stock = mongoose.model("Stock", stockSchema);
   
   app.route('/api/stock-prices')
-    .get(function (req, res){
-    var stock1 = req.query.stock1.toUpperCase();
-    var stock2;          //if stock2 compare stock prices
-    if (req.query.stock2) { stock2 = req.query.stock2.toUpperCase(); }
-    var like = req.query.like ? 1 : 0;
-    var ip = like ? req.ip : null;
-    var stockPrice;
-    var responseStock = [];
+    .get(function (req, res) {
+      var stock1 = req.query.stock1.toUpperCase().trim();
+      var stock2;          //if stock2 compare stock prices
+      if (req.query.stock2) { stock2 = req.query.stock2.toUpperCase().trim(); }
+      var like = req.query.like ? 1 : 0;
+      var ip = like ? req.ip : null;
+      var stockPrice;
+      var responseStock = [];
 
-    var addNewStock = (stock) => {
-      return new Promise((resolve, reject) => { 
-      var newStock = new Stock({stock: stock, price: stockPrice, likes: like});
-      console.log(newStock);
-      newStock.save( (err, doc) => {
-        if (err) { console.log(err); }
-        else if (!doc) { console.log("addNewStock failed")} 
-        else {
-          console.log("addNewStock was a success"); 
-          responseStock.push({"stock": doc.stock, "price": doc.price, "likes": doc.likes});
-          console.log(responseStock);
-          resolve();
-        }
-      });
-    }); // end Promise
-    };
-    
-    var updateStockPriceAndLikes = (stock) => {
-      return new Promise((resolve, reject) => { 
-        Stock.findOneAndUpdate({stock: stock}, {price: stockPrice, $inc: {likes: like}, $push: {ip: ip}},
-                             {new: true}, function(err, doc) {
-        if (err) { console.log(err); }
-        else if (!doc) { console.log("updateStockPriceAndLikes failed"); }
-        else { 
-          console.log("updateStockPriceAndLikes was a success"); 
-          responseStock.push({"stock": doc.stock, "price": doc.price, "likes": doc.likes});
-          console.log(responseStock);
-          resolve();
-        }
-      })
-    }) // end Promise
-    };
-    
-    var updateStockPrice = (stock) => {
-      return new Promise((resolve, reject) => {
-      console.log("stockPrice = " + stockPrice)
-      Stock.findOneAndUpdate({stock: stock}, {price: stockPrice},
-                             {new: true}, function(err, doc) {
-        if (err) { console.log(err); }
-        else if (!doc) { console.log("updateStockPrice failed"); }
-        else { 
-          console.log("updateStockPrice was a success");
-          responseStock.push({"stock": doc.stock, "price": doc.price, "likes": doc.likes});
-          console.log(responseStock);
-          resolve();
-        }
-      })
-    }) // end Promise
-    };
+      var addNewStock = (stock) => {
+        return new Promise((resolve, reject) => { 
+          var newStock = new Stock({stock: stock, price: stockPrice, likes: like});
+          console.log(newStock);
+          newStock.save( (err, doc) => {
+            if (err) { 
+              console.log(err);
+              res.send("Something went wrong.  Please try again.");
+              reject(new Error("Error saving stock")); 
+            } else if (!doc) { 
+              res.send("Something went wrong.  Please try again.");
+              reject(new Error("New stock was not saved"));
+            } else {
+              console.log("addNewStock was a success"); 
+              responseStock.push({"stock": doc.stock, "price": doc.price, "likes": doc.likes});
+              console.log(responseStock);
+              resolve();
+            }
+          });
+        }); // end Promise
+      };
       
-    var handleStock = (stock) => {
-      return new Promise((resolve, reject) => { 
-      if (stock) {
-       if (ip) {       // like is checked
-       Stock.findOne({stock: stock}, async function(err, doc) {
-          if (err) { console.log(err); }
-          else if (!doc) { 
-            await addNewStock(stock);   //not in db, add new
-            resolve(); 
-          } else if (doc.ip.indexOf(ip) < 0) {    //ip not found
-            await updateStockPriceAndLikes(stock);     //and push ip to db
-            resolve();
-          } else {
-            await updateStockPrice(stock);
-            resolve();
-          }
-        })
-       } else if (!ip) {   //like is not checked
-        Stock.findOne({stock: stock}, async function(err, doc) {
-          if (err) { console.log(err); }
-          else if (!doc) {
-            await addNewStock(stock);
-            resolve();
-          } else {
-            await updateStockPrice(stock);
-            resolve();
-          }
-        });
-      }
-    };
-    })  
-  }; 
-    
-    var getStockPrice = (stock) => {  
-      var url = "https://api.iextrading.com/1.0/stock/" + stock + "/book";
+      var updateStockPriceAndLikes = (stock) => {
+        return new Promise((resolve, reject) => { 
+          Stock.findOneAndUpdate({stock: stock}, {price: stockPrice, $inc: {likes: like}, $push: {ip: ip}},
+                                {new: true}, function(err, doc) {
+            if (err) { 
+              console.log(err); 
+              res.send("Something went wrong.  Please try agin.");
+              reject(new Error("Error: could not update stock price and likes"));
+            }
+            else if (!doc) { 
+              console.log("updateStockPriceAndLikes failed"); 
+              res.send("Something went wrong.  Please try agin.");
+              reject(new Error("Stock price and likes were not updated"));
+            }
+            else { 
+              console.log("updateStockPriceAndLikes was a success"); 
+              responseStock.push({"stock": doc.stock, "price": doc.price, "likes": doc.likes});
+              console.log(responseStock);
+              resolve();
+            }
+          })
+        }) // end Promise
+      };
+      
+      var updateStockPrice = (stock) => {
+        return new Promise((resolve, reject) => {
+          console.log("stockPrice = " + stockPrice)
+          Stock.findOneAndUpdate({stock: stock}, {price: stockPrice},
+                                {new: true}, function(err, doc) {
+            if (err) { 
+              console.log(err); 
+              res.send("Something went wrong.  Please try agin.");
+              reject(new Error("Error: could not update stock price"));
+            } else if (!doc) { 
+              console.log("updateStockPrice failed"); 
+              res.send("Something went wrong.  Please try agin.");
+              reject(new Error("Stock price was not updated"));
+            }
+            else { 
+              console.log("updateStockPrice was a success");
+              responseStock.push({"stock": doc.stock, "price": doc.price, "likes": doc.likes});
+              console.log(responseStock);
+              resolve();
+            }
+          })
+        }) // end Promise
+      };
+        
+      var handleStock = (stock) => {
+        return new Promise((resolve, reject) => { 
+          if (stock) {
+            if (ip) {       // like is checked
+              Stock.findOne({stock: stock}, async function(err, doc) {
+                if (err) { 
+                  console.log(err); 
+                  res.send("Sorry an error occured.  Please try again.");
+                  reject(new Error("Error finding stock with like checked"));
+                }
+                else if (!doc) {     //not in db, add new
+                  await addNewStock(stock);   
+                  resolve(); 
+                } else if (doc.ip.indexOf(ip) < 0) {    //ip not found
+                  await updateStockPriceAndLikes(stock);     //and push ip to db
+                  resolve();
+                } else {
+                  await updateStockPrice(stock);
+                  resolve();
+                }
+              })
+            } else if (!ip) {   //like is not checked
+              Stock.findOne({stock: stock}, async function(err, doc) {
+                if (err) { 
+                  console.log(err); 
+                  res.send("Sorry an error occured.  Please try again.");
+                  reject(new Error("Error finding stock"));
+                }
+                else if (!doc) {
+                  await addNewStock(stock);
+                  resolve();
+                } else {
+                  await updateStockPrice(stock);
+                  resolve();
+                }
+              });
+            }
+          };
+        })  
+      }; 
+      
+      var getStockPrice = (stock) => {  
+        var url = "https://api.iextrading.com/1.0/stock/" + stock + "/book";
         return new Promise( (resolve, reject) => { 
           request(url, {json: true}, async function(err, resp, body) {
             console.log("body: " + body)
-            if (err) { console.log(err); }
-            else if (!body) {
-              res.send("please enter a valid stock");
-              reject();
+            if (err) { 
+              console.log(err); 
+              reject(new Error("Stock Request Error"));
+            } else if (body === "Unknown symbol") {
+              res.send(`${stock} is not a valid stock symbol.`);
+              reject(new Error("Invalid Stock"));
             } else {
               console.log("latestPrice: " + body["quote"])
               stockPrice = body["quote"].latestPrice;
@@ -138,25 +165,35 @@ module.exports = function (app) {
             }
           })
         }) // end Promise
-      //}
-    };
-    
-    var sendResponse = (response) => {
-      if (responseStock.length > 1) {   //user entered 2 stocks to compare
-        var likes0 = response[0].likes - response[1].likes;   //compare relative likes
-        var likes1 = response[1].likes - response[0].likes;
-        res.json({"stockData": [{"stock": response[0].stock, "price": response[0].price, "rel_likes": likes0},
-                               {"stock": response[1].stock, "price": response[1].price, "rel_likes": likes1}]});
-      } else {
-        res.json({"stockData": response});
       };
-    };
-    
-    (async () => {  
-      await getStockPrice(stock1);
-      if (stock2) { await getStockPrice(stock2); }  
-      sendResponse(responseStock);
-    })();
+      
+      var sendResponse = (response) => {
+        if (responseStock.length > 1) {   //user entered 2 stocks to compare
+          var likes0 = response[0].likes - response[1].likes;   //compare relative likes
+          var likes1 = response[1].likes - response[0].likes;
+          res.json({"stockData": [{"stock": response[0].stock, "price": response[0].price, "rel_likes": likes0},
+                                {"stock": response[1].stock, "price": response[1].price, "rel_likes": likes1}]});
+        } else {
+          res.json({"stockData": response});
+        };
+      };
+      
+      (async () => {  
+        let promiseError;
+        await getStockPrice(stock1).catch( err => {
+          console.log("stock price1 error: " + err);
+          promiseError = err;
+        });
+        if (promiseError) return;
+        if (stock2) { 
+          await getStockPrice(stock2).catch( err => {
+            console.log("stock price2 error: " + err);
+            promiseError = err;
+          }); 
+        }  
+        if (promiseError) return;
+        sendResponse(responseStock);
+      })();
                                   
     });
     
